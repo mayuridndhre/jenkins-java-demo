f60e64586d3381a96091c23cecbf7e9643f418c0
Mayuri Dandhare <mayuridandhare62@gmail.com>
2025-08-30 02:51:49 +0530

    Updated HelloWorld message

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,6 @@ class HelloWorld
 public static void main(String args[])
 {
 System.out.println("Hello World");
-System.out.println("Hello Mahi");
+System.out.println("Hello from Jenkins with github Integration!");
 }
 }
